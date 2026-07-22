94bb5930274a063d24e53a8057c5dd056df67d99
Trisert <nicola@trisert.dev>
2026-07-22 16:59:55 +0200

    remove walk page entirely

--- walk/js/test_terrain.mjs
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env node
-import { terrainHeight, WATER_LEVEL } from './terrain.js';
-
-for (const [x, z] of [[0,0],[0,2],[0,5],[0,8],[0,10],[0,12],[0,15],[0,18],[0,20],[0,25],[0,30],[5,0],[-5,0],[0,-5]]) {
-  console.log(`terrainAt(${x},${z}) = ${terrainHeight(x,z).toFixed(2)}`);
-}
-console.log(`WATER_LEVEL = ${WATER_LEVEL}`);
